f2e98ade453b979b178336beb12c2867517b5fee
hiresfilip <46921093+hiresfilip@users.noreply.github.com>
2020-07-21 18:04:26 +0200

    Add files via upload

--- js/version.js
+++ js/version.js
@@ -1,6 +1,8 @@
-console.log('Verze 4.0 - Poděbrady - upgrade: 20.7.2020 9:35');
+console.log('Verze 5.0 - Poděbrady - upgrade: 21.7.2020 18:02');
 /* Verze 1.1 - Poděbrady - update: 16.7.2020 18:22 */
 /* Verze 1.2 - Poděbrady - update: 19.7.2020 16:52 */
 /* Verze 2.1 - Poděbrady - upgrade: 19.7.2020 16:53 */
 /* Verze 2.2 - Poděbrady - update: 19.7.2020 19:20*/
 /* Verze 3.0 - Poděbrady - upgrade: 20.7.2020 9:25*/
+/* Verze 4.0 - Poděbrady - upgrade: 20.7.2020 9:35 */
+/* Verze 4.1 - Poděbrady - upgrade: 21.7.2020 16:41 */
