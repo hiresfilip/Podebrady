955e998009efbfe3706795c3ed61fee924b3e8d7
hiresfilip <46921093+hiresfilip@users.noreply.github.com>
2022-03-18 11:32:07 +0100

    Add files via upload

--- js/version.js
+++ js/version.js
@@ -1,10 +1,14 @@
-console.log('Verze 6.1 - Poděbrady - update: 24.7.2020 14:00');
-/* Verze 1.1 - Poděbrady - update: 16.7.2020 18:22 */
-/* Verze 1.2 - Poděbrady - update: 19.7.2020 16:52 */
-/* Verze 2.1 - Poděbrady - upgrade: 19.7.2020 16:53 */
-/* Verze 2.2 - Poděbrady - update: 19.7.2020 19:20*/
-/* Verze 3.0 - Poděbrady - upgrade: 20.7.2020 9:25*/
-/* Verze 4.0 - Poděbrady - upgrade: 20.7.2020 9:35 */
-/* Verze 4.1 - Poděbrady - upgrade: 21.7.2020 16:41 */
-/* Verze 5.0 - Poděbrady - upgrade: 21.7.2020 18:02 */
+console.log('Verze 6.3 - Poděbrady - update: 18.3.2022 11:30');
+console.log('havirsky_kostel.html, video je soukromé !!!');
+
+/* Verze 6.2 - Poděbrady - update: 4.3.2022 12:36 */
+/* Verze 6.1 - Poděbrady - upgrade: 24.7.2020 14:00 */
 /* Verze 6.0 - Poděbrady - upgrade: 21.7.2020 20:20 */
+/* Verze 5.0 - Poděbrady - upgrade: 21.7.2020 18:02 */
+/* Verze 4.1 - Poděbrady - upgrade: 21.7.2020 16:41 */
+/* Verze 4.0 - Poděbrady - upgrade: 20.7.2020 9:35 */
+/* Verze 3.0 - Poděbrady - upgrade: 20.7.2020 9:25*/
+/* Verze 2.2 - Poděbrady - update: 19.7.2020 19:20*/
+/* Verze 2.1 - Poděbrady - upgrade: 19.7.2020 16:53 */
+/* Verze 1.2 - Poděbrady - update: 19.7.2020 16:52 */
+/* Verze 1.1 - Poděbrady - update: 16.7.2020 18:22 */
